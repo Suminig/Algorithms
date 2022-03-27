8ed126c4425b6f79b182097d2fc3ebc570bff6f7
이수민 <leesumin@LEEsuminui-MacBook-Pro.local>
2022-03-27 23:38:20 +0900

    update

---
+++ BOJ/14501.py
@@ -0,0 +1,30 @@
+# https://www.acmicpc.net/problem/14501
+# Silver III
+
+import sys
+sys.setrecursionlimit(10**6)
+n = int(sys.stdin.readline())
+t = []
+p = []
+arr = []
+
+for i in range(n):
+    ti, pi = sys.stdin.readline().rstrip().split()
+    t.append(int(ti))
+    p.append(int(pi))
+
+
+def dfs(day, earn):
+    if day >= n:
+        arr.append(earn)
+        return
+    # 현재 day + 소요 day가 n보다 크면 상담을 하지 않는 조건을 붙여준다
+    if (day+t[day]) <= n:
+        # 현재 day에서 상담을 진행하는 경우
+        dfs(day+t[day], earn+p[day])
+    # 현재 day에서 상담을 진행하지 않는 경우
+    dfs(day+1, earn)
+
+
+dfs(0, 0)
+print(max(arr))
