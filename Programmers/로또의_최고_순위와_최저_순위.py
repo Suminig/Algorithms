388ae70f992d56c6a041928a0684bdd410b23139
이수민 <leesumin@LEEsuminui-MacBook-Pro.local>
2022-03-28 12:57:26 +0900

    Update

---
+++ Programmers/로또의_최고_순위와_최저_순위.py
@@ -0,0 +1,19 @@
+# https://programmers.co.kr/learn/courses/30/lessons/77484
+# LEVEL 1
+
+def solution(lottos, win_nums):
+    answer = []
+    rank = [6, 6, 5, 4, 3, 2, 1]
+    unknown = lottos.count(0)
+    same = 0
+
+    for lotto in lottos:
+        if lotto in win_nums:
+            same += 1
+
+    # 최고 순위: 0의 갯수 + 맞은 번호의 개수
+    answer.append(rank[unknown+same])
+    # 최저 순위: 맞은 번호의 개수
+    answer.append(rank[same])
+
+    return answer
