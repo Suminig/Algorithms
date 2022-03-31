cd8f76298309568e2de0b79d9fbfa8a981f55378
이수민 <leesumin@LEEsuminui-MacBook-Pro.local>
2022-03-31 23:49:14 +0900

    Update

---
+++ BOJ/5014.py
@@ -0,0 +1,26 @@
+# https://www.acmicpc.net/problem/5014
+# GOLD V
+
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+F, S, G, U, D = map(int, input().split())
+visited = [0] * (F+1)
+q = deque()
+q.append(S)
+visited[S] = 1
+
+while q:
+    cur = q.popleft()
+    if cur+U <= F and visited[cur+U] == 0:
+        q.append(cur+U)
+        visited[cur+U] = visited[cur] + 1
+    if cur-D > 0 and visited[cur-D] == 0:
+        q.append(cur-D)
+        visited[cur-D] = visited[cur] + 1
+
+if visited[G] > 0:
+    print(visited[G]-1)
+else:
+    print("use the stairs")
