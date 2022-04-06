8568121250d47a8671115007f7bba1eb01d16fb3
Sumin Lee <qwemnbv98@outlook.com>
2022-04-06 23:16:24 +0900

    구간 합 구하기 5

---
+++ BOJ/11660.py
@@ -0,0 +1,20 @@
+# https://www.acmicpc.net/problem/11660
+# 구간 합 구하기 5
+# SILVER I
+
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+dp = [[0]*(N+1) for _ in range(N+1)]
+
+# 합 더해주는 dp배열 만들기
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + arr[i-1][j-1]
+
+# x1~x2, y1~y2구간 합 = x2,y2 - x1-1,y2 - x2,y1-1 + x1-1,y1-1 (위와 왼쪽 구간 합 빼준 뒤 겹치는 부분 만큼 더해 줌)
+for _ in range(M):
+    x1, y1, x2, y2 = map(int, input().split())
+    print(dp[x2][y2]-dp[x1-1][y2]-dp[x2][y1-1]+dp[x1-1][y1-1])
