4be16363b77dcace21d1cc19ee5285090ab63ce3
Sumin Lee <qwemnbv98@outlook.com>
2022-07-30 21:51:30 +0900

    feat: Add BOJ-1302-Java

--- Java/BOJ/BOJ_1302.java
+++ Java/BOJ/BOJ_1302.java
@@ -1,32 +1,37 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(in.readLine());
-		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		Map<String, Integer> map = new HashMap<String, Integer>();
 
 		for (int i = 0; i < N; i++) {
 			String book = in.readLine();
 			if (map.containsKey(book))
-				map.put(book, map.get(book)+1);
+				map.put(book, map.get(book) + 1);
 			else
 				map.put(book, 1);
 		}
-		
-    // 수가 같으면 사전순에서 젤 첫번째 출력해야함 
+
+		String[] books = new String[map.keySet().size()];
+		map.keySet().toArray(books);
+		Arrays.sort(books);
+
 		int max = 0;
 		String maxBook = "";
-		for (String b : map.keySet()) {
+		for (String b : books) {
 			if (map.get(b) > max) {
 				max = map.get(b);
 				maxBook = b;
 			}
 		}
-
 		System.out.println(maxBook);
 	}
-
 }
