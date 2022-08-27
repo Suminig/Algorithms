75e4f0c0015862d453bd99470733c1f443e0a7c9
Sumin Lee <qwemnbv98@outlook.com>
2022-08-27 19:17:12 +0900

    feat: Add BOJ-11404-Java

---
+++ Java/BOJ/GOLD IV/Main_11404_플로이드.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	private static StringTokenizer st;
+	private static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(in.readLine());
+		int[][] d = new int[N + 1][N + 1];
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				d[i][j] = 987654321;
+
+				if (i == j)
+					d[i][j] = 0;
+			}
+		}
+
+		int M = Integer.parseInt(in.readLine());
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(in.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			d[a][b] = Math.min(d[a][b], c);
+		}
+
+		for (int k = 1; k <= N; k++) {
+			for (int i = 1; i <= N; i++) {
+				for (int j = 1; j <= N; j++) {
+					d[i][j] = Math.min(d[i][j], d[i][k] + d[k][j]);
+				}
+			}
+		}
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				if (d[i][j] == 987654321)
+					d[i][j] = 0;
+
+				sb.append(d[i][j]).append(" ");
+			}
+			sb.append("\n");
+		}
+		out.write(sb.toString());
+		out.flush();
+		out.close();
+	}
+}
