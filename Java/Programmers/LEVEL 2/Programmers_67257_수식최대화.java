56692aca569e60adf165546c31a9297fb56b45a4
Sumin Lee <qwemnbv98@outlook.com>
2022-09-08 00:26:05 +0900

    feat: Add Programmers-67257-수식최대화-Java

--- Java/Programmers/LEVEL 2/Programmers_67257_수식최대화.java
+++ Java/Programmers/LEVEL 2/Programmers_67257_수식최대화.java
@@ -1,51 +1,68 @@
 import java.util.ArrayList;
 
 class Solution {
-    private long maxNum = 0;
-    private String[][] priorities = {{"+","-","*"}, {"+","*","-"}, {"-","+","*"}, {"-","*","+"}, {"*","-","+"}, {"*","+","-"}};             
-    
-    public long solution(String expression) {
-        for(int i = 0; i < 6; i++){
-            StringBuilder sb = new StringBuilder();
-            ArrayList<Long> numList = new ArrayList<>();
-            ArrayList<String> operList = new ArrayList<>();
-            
-            for(int j = 0, size = expression.length(); j < size; j++){
-                if(expression.charAt(j) == '+' || expression.charAt(j) == '-' || expression.charAt(j) == '*'){
-                    numList.add(Long.parseLong(sb.toString()));
-                    sb = new StringBuilder();
-                    operList.add(String.valueOf(expression.charAt(j)));
-                }else{
-                    sb.append(expression.charAt(j));
-                }
-                
-                if(!sb.toString().equals(""))
-                    numList.add(Long.parseLong(sb.toString()));
-                    
-                for(int k = 0; k < 3; k++){
-                    String priority = priorities[i][k];
-                    
-                    while(!operList.isEmpty()){
-                        int index = operList.indexOf(priority);
-                        
-                        if(index == -1) break;
-                        else{
-                            if(priority.equals("+"))
-                                numList.add(index, numList.get(index)+numList.get(index+1));
-                            else if(priority.equals("+"))
-                                numList.add(index, numList.get(index)-numList.get(index+1));
-                            else
-                                numList.add(index, numList.get(index)*numList.get(index+1));
-                            
-                            numList.remove(index+1);
-                            numList.remove(index+1);
-                            operList.remove(index);
-                        }
-                    }   
-                }
-                maxNum = Math.max(maxNum, Math.abs(numList.get(0)));
-            }
-        }
-        return maxNum;
-    }
+	private long maxNum = 0;
+	private String expression;
+	private String[] opers = { "+", "-", "*" };
+
+	public void dfs(int cnt, int flag, String priority) {
+		if (cnt == 3) {
+			maximize(priority);
+			return;
+		}
+
+		for (int i = 0; i < 3; i++) {
+			if ((flag & 1 << i) != 0)
+				continue;
+
+			dfs(cnt + 1, flag | 1 << i, priority + opers[i]);
+		}
+	}
+
+	public void maximize(String priority) {
+		StringBuilder sb = new StringBuilder();
+		ArrayList<Long> numList = new ArrayList<>();
+		ArrayList<String> operList = new ArrayList<>();
+
+		for (int i = 0, size = expression.length(); i < size; i++) {
+			if (expression.charAt(i) == '+' || expression.charAt(i) == '-' || expression.charAt(i) == '*') {
+				numList.add(Long.parseLong(sb.toString()));
+				sb = new StringBuilder();
+				operList.add(String.valueOf(expression.charAt(i)));
+			} else {
+				sb.append(expression.charAt(i));
+			}
+		}
+
+		numList.add(Long.parseLong(sb.toString()));
+
+		for (int i = 0; i < 3; i++) {
+			String oper = String.valueOf(priority.charAt(i));
+
+			while (!operList.isEmpty()) {
+				int index = operList.indexOf(oper);
+
+				if (index == -1)
+					break;
+
+				if (oper.equals("+"))
+					numList.add(index, numList.get(index) + numList.get(index + 1));
+				else if (oper.equals("-"))
+					numList.add(index, numList.get(index) - numList.get(index + 1));
+				else
+					numList.add(index, numList.get(index) * numList.get(index + 1));
+
+				numList.remove(index + 1);
+				numList.remove(index + 1);
+				operList.remove(index);
+			}
+		}
+		maxNum = Math.max(maxNum, Math.abs(numList.get(0)));
+	}
+
+	public long solution(String expression) {
+		this.expression = expression;
+		dfs(0, 0, "");
+		return maxNum;
+	}
 }
